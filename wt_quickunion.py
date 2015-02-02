dae9f19900003d3c6a71b54304bf63ec40a2a4a1
soham26 <soham.sg@gmail.com>
2015-02-02 16:16:18 +0530

    wt_quickunion
    A weighted quick union algorithm, implemented to keep check on the length of trees formed. Long trees are avoided here.

---
+++ wt_quickunion.py
@@ -0,0 +1,41 @@
+"""
+Weighted quick union to avoid larger trees. Here, always the smaller tree is added to the larger tree, hence max distance of any node from root is less than what we find in the normal implementation of quick union.
+
+Here we keep track of no. of objects in trees!! We connect any tree, object to the main parent roots! and the smaller is joined with the larger!
+"""
+
+N=input('Enter the no. of nodes \n')
+rt={} # dictionary of nodes and roots
+
+#  A root is one which has same rt[i]==i
+
+for i in range(N):
+	rt[i]=i
+	# initial indexing done
+
+def root_node(p):
+	while(rt[p]!=p):
+		p=rt[p]
+	return p   # returns the root of the tree
+		
+def isconnected(child,parent):
+	#check until rt[child]==rt[parent]
+	if root_node(child)==root_node(parent):
+		return True
+	else:
+		return False
+
+# to find size of each node tree to be added to another.
+def size(p):
+	p_root=root_node(p)
+	sizeof_tree=0
+	for i in range(N):
+		if root_node(i)==p_root:
+			sizeof_tree+=1
+	return sizeof_tree
+		
+def union(child,parent):
+	if size(child)<size(parent):
+		rt[root_node(child)]=root_node(parent)
+	else:
+		rt[root_node(parent)]=root_node(child)
